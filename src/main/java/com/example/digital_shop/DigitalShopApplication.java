8883841116c4fb69e88774bf958738ab1d26d0a3
Mr.Coder <elmurodov1106@gmail.com>
2023-09-11 15:51:59 +0500

    Proyect manalidga o'girildi

---
+++ src/main/java/com/example/digital_shop/DigitalShopApplication.java
@@ -0,0 +1,13 @@
+package com.example.digital_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DigitalShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/digital_shop/DigitalShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.digital_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
