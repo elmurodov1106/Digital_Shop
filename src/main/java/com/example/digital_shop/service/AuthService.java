1978d635708f444e53d3b53bade91d61a94cd452
Mr.Coder <elmurodov1106@gmail.com>
2023-09-21 15:35:56 +0500

    hatola qisman to'g'irlandi

--- src/main/java/com/example/digital_shop/service/AuthService.java
+++ src/main/java/com/example/digital_shop/service/AuthService.java
@@ -1,5 +1,6 @@
 package com.example.digital_shop.service;
 
+import com.example.digital_shop.entity.user.UserEntity;
 import com.example.digital_shop.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -13,6 +14,11 @@ public class AuthService implements UserDetailsService {
     private final UserRepository userRepository;
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserEntity user = userRepository.findByEmail(username);
+
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found with username: " + username);
+        }
         return  userRepository.findByEmail(username);
 
     }
