eb92c1a62247f1f73f8b3c3754ba7c010a74e2d7
Yusupov Ulugbek <yusupovulugbek73@gmail.com>
2023-09-30 12:52:08 +0500

    sss

--- src/main/java/com/example/digital_shop/domain/dto/LaptopDto.java
+++ src/main/java/com/example/digital_shop/domain/dto/LaptopDto.java
@@ -2,7 +2,6 @@ package com.example.digital_shop.domain.dto;
 
 import jakarta.validation.constraints.NotEmpty;
 import lombok.*;
-import org.springframework.web.multipart.MultipartFile;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -10,7 +9,12 @@ import org.springframework.web.multipart.MultipartFile;
 @Setter
 @Builder
 public class LaptopDto {
+    @NotEmpty(message = "name cannot be empty")
+    private String name;
 
+    @NotEmpty(message = "model cannot be empty")
+    private String model;
+    private Integer battery;
     @NotEmpty(message = "colour cannot be empty")
     private String colour;
 
@@ -28,11 +32,6 @@ public class LaptopDto {
 
     @NotEmpty(message = "Ghz cannot be empty")
     private Integer Ghz;
-    @NotEmpty(message = "name cannot be empty")
-    private String name;
-
-    @NotEmpty(message = "model cannot be empty")
-    private String model;
 
     @NotEmpty(message = "cost cannot be empty")
     private Double cost;
