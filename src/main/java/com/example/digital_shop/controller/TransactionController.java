fe24d8d1e472568f2be6d09539e45dd48c6be70d
abduvohidovismoil <abduvohidovi72@gmail.com>
2023-10-10 17:18:39 +0500

    TransactionController update

--- src/main/java/com/example/digital_shop/controller/TransactionController.java
+++ src/main/java/com/example/digital_shop/controller/TransactionController.java
@@ -1,9 +1,6 @@
 package com.example.digital_shop.controller;
 
-import com.example.digital_shop.config.CookieValue;
-import com.example.digital_shop.domain.dto.TransactionDto;
 import com.example.digital_shop.service.transaction.TransactionService;
-import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -14,32 +11,6 @@ import java.util.UUID;
 @RequestMapping("/transaction")
 public class TransactionController {
     private final TransactionService transactionService;
-//    @PostMapping("/save")
-//    public String save(
-//            @RequestBody TransactionDto transactionDto,
-//            HttpServletRequest request
-//    ){
-//        UUID userId=UUID.fromString(CookieValue.getValue("userId",request));
-//        transactionService.save(transactionDto,userId);
-//        return "";
-//    }
-    @GetMapping("/getAllTransactions")
-    public String getAllTransaction(
-            @RequestParam(required = false) int page,
-            @RequestParam(required = false) int size
-    ){
-        transactionService.getAllTransactions(page,size);
-        return "";
-    }
-    @DeleteMapping("/delete")
-    public String delete(
-            @RequestParam UUID transactionId,
-            HttpServletRequest request
-            ){
-        UUID userId=UUID.fromString(CookieValue.getValue("userId",request));
-        transactionService.deleteById(transactionId,userId);
-        return "";
-    }
 
     @PutMapping("/transaction")
     public String p2p(@RequestParam UUID sender,
